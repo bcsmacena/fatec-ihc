528fc2b7e6fb557321eb9efff43ae43fdbcfcd34
Bruno Macena <macena@outlook.com>
2020-07-05 18:19:55 -0300

    Fluxo de cadastro empresa

--- controllers/cadastroEmpresaController.js
+++ controllers/cadastroEmpresaController.js
@@ -1,17 +1,35 @@
+const { Empresa } = require("../models");
+const bcrypt = require("bcrypt");
+
 const cadastroEmpresaController = {
     index: async(req,res) => {
         res.render('cadastro/empresa');
     },
     store: async(req,res) => {
-        const { nome, cnpj, telefone, email, senha } = req.body
-        const novaEmpresa = {
-            nome,
-            cnpj,
-            telefone,
-            email,
-            senha
+        const { cnpj, razaoSocial, nomeFantasia, atuacao, cep, endereco, complemento, bairro, cidade, estado, telefone, responsavel, email, senha } = req.body
+
+        try{
+            const empresa = await Empresa.create({
+                cnpj,
+                razaoSocial,
+                nomeFantasia,
+                atuacao,
+                cep,
+                endereco,
+                complemento,
+                bairro,
+                cidade,
+                estado,
+                telefone,
+                responsavel,
+                email,
+                senha
+            })
+            res.send(empresa);
         }
-        res.send(novaEmpresa);
+        catch(e){
+            res.send(e);
+        }
     },
 }
 
