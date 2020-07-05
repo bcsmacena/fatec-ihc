e19700bde9cce1147ccd1cacd68fafa4f247b019
Bruno Macena <macena@outlook.com>
2020-07-05 18:19:20 -0300

    Validação do cep com focusout

--- public/javascripts/validadorCep.js
+++ public/javascripts/validadorCep.js
@@ -9,9 +9,47 @@ inputCep.addEventListener("keypress", (e) => {
   }
 })
 
+inputCep.addEventListener("focusout", (e) => {
+  preencheCep();
+})
+
 
 btnPesquisarCEP.addEventListener("click", event =>{
   event.preventDefault();
+  preencheCep();
+  // const inputDoCep = document.querySelector("#cep");
+  // const valorDoCep = inputDoCep.value;
+  // const url = `https://viacep.com.br/ws/${valorDoCep}/json/`;
+  // fetch(url).then(response =>{
+  //   return response.json();
+  //     }).then(data =>
+  //   {
+  //   if(data.erro)
+  //   {
+  //   alert("O CEP DIGITADO ESTÁ INVÁLIDO");
+  //   return ;
+  //   }
+  //   atribuirCampos(data);
+  //  })
+
+  // function atribuirCampos(data){
+        
+  //   const bairro = document.querySelector("#bairro");
+  //   const cidade = document.querySelector("#cidade");
+  //   const estado = document.querySelector("#estado");
+  //   const endereco = document.querySelector("#endereco");
+    
+  //   bairro.value = data.bairro;
+  //   cidade.value = data.localidade;
+  //   estado.value = data.uf;
+  //   endereco.value = data.logradouro;
+        
+  // }
+
+})
+
+function preencheCep(){
+
   const inputDoCep = document.querySelector("#cep");
   const valorDoCep = inputDoCep.value;
   const url = `https://viacep.com.br/ws/${valorDoCep}/json/`;
@@ -22,30 +60,27 @@ btnPesquisarCEP.addEventListener("click", event =>{
     if(data.erro)
     {
     alert("O CEP DIGITADO ESTÁ INVÁLIDO");
+    inputCep.value = "";
     return ;
     }
     atribuirCampos(data);
    })
 
-  function atribuirCampos(data)
-        {
-
+  function atribuirCampos(data){
         
-        const bairro = document.querySelector("#bairro");
-        const cidade = document.querySelector("#cidade");
-        const estado = document.querySelector("#estado");
-        const endereco = document.querySelector("#endereco");
+    const bairro = document.querySelector("#bairro");
+    const cidade = document.querySelector("#cidade");
+    const estado = document.querySelector("#estado");
+    const endereco = document.querySelector("#endereco");
     
+    bairro.value = data.bairro;
+    cidade.value = data.localidade;
+    estado.value = data.uf;
+    endereco.value = data.logradouro;
         
-        bairro.value = data.bairro;
-        cidade.value = data.localidade;
-        estado.value = data.uf;
-        endereco.value = data.logradouro;
-        
-                }
+  }
 
-        })
-
+}
   
 
 
