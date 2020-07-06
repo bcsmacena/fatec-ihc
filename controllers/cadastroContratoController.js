e6092f6f7b5dea748e2d440cfe05be4d72a14e18
Bruno Macena <macena@outlook.com>
2020-07-06 12:34:43 -0300

    Melhorias no fluxo de contrato

--- controllers/cadastroContratoController.js
+++ controllers/cadastroContratoController.js
@@ -1,4 +1,5 @@
 const { Colaborador, Empresa, Contrato } = require('../models')
+const Sequelize = require('sequelize');
 
 const cadastroContratoController = {
     index: async(req,res) => {
@@ -6,7 +7,6 @@ const cadastroContratoController = {
         try
         {
             const colaboradores = await Colaborador.findAll();
-            console.log(colaboradores);
             return res.render('cadastro/contrato', { colaboradores, idLogado });
         }
         catch(e)
@@ -18,10 +18,18 @@ const cadastroContratoController = {
     store: async(req,res) => {
 
         const { cpf, nome, telefone, email, funcao, inicioJornada, terminoJornada, valorHora, descricao, idLogado } = req.body
-        
+    
         try{
             const colaborador = await Colaborador.findOne({ where: { cpf: cpf}});
-            
+
+            const colaboradores = await Colaborador.findAll();
+
+            const buscaContrato = await Contrato.findOne({ where: Sequelize.and({colaborador_id: colaborador.id },{empresa_id: idLogado } )})
+
+            if(buscaContrato) {
+                return res.render('cadastro/contrato', { msg: "CPF já possui contrato", colaboradores, idLogado})
+            }
+
             if(colaborador){
                 const novoContrato = await Contrato.create({
                     empresa_id: idLogado,
@@ -35,11 +43,12 @@ const cadastroContratoController = {
                     valorHora,
                     descricao
                 })
-                console.log(novoContrato)
+                // console.log(novoContrato)
                 return res.render('empresa', { msg: "Novo contrato cadastrado com sucesso"});
             } else {
                 const idLogado = req.session.user.id;
-                return res.render('cadastro/contrato', { msg: "CPF inválido ou ainda não foi cadastrado no sistema", idLogado})
+                const colaboradores = await Colaborador.findAll();
+                return res.render('cadastro/contrato', { msg: "CPF inválido ou ainda não foi cadastrado no sistema", colaboradores, idLogado})
             }
         }
         catch(e){
